c2f4040fe66c7b44faecd9461ab574dd4944e141
kerusey <38439184+kerusey@users.noreply.github.com>
2019-06-28 13:16:01 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,3 +1,17 @@
+/*	!!! important stuff:
+
+magnet_objects 1 and 2 used for X axis (right) (left)
+magnet_objects 3 and 4 used for Y axis (right) (left)
+
+ultrasonic objects 1 and 2 used for X axis (right) (left)
+ultrasonic objects 3 and 4 used for X axis (right) (left)
+
+!!!*/
+
+
+
+
+
 #include "Ultrasonic.h"
 
  // globals ***************************************
@@ -34,13 +48,63 @@
 
  // 4 ultrasonic objects
  
-Ultrasonic ultrasonic1(Trig1, Echo1),
-           ultrasonic2(Trig2, Echo2), // these are for x - axis
+Ultrasonic ultrasonic_object1 (Trig1, Echo1),
+           ultrasonic_object2 (Trig2, Echo2), // these are for x - axis
 
-           ultrasonic3(Trig3, Echo3),
-           ultrasonic4(Trig4, Echo4); // these - for y- axis
+           ultrasonic_object3 (Trig3, Echo3),
+           ultrasonic_object4 (Trig4, Echo4); // these - for y- axis
 
 // resisto-coil class provide control coils via changing resistance of controllable resistor that been paralleled with the coil
+
+ class axis { // triangle sights a & b ; c - const
+protected:
+
+  int Adist;
+  int Bdist;
+  Ultrasonic *ultra_object1;
+  Ultrasonic *ultra_object2;
+
+public:
+
+  void read_A (){
+    Adist = ultra_object1->distanceRead();
+  }
+
+  void read_B (){
+    Bdist = ultra_object2->distanceRead();
+  }
+
+  
+  axis(Ultrasonic &ultra_object1, Ultrasonic &ultra_object2) {
+    read_A();
+    read_B();
+
+    } // constructor
+
+  int check_axis_orientary () { // returns the axis inclination (from the right sight)
+    return (Adist - Bdist); 
+  }
+
+ };
+
+struct double_result {
+  int result1;
+  int result2;
+
+  const double_result operator = (double_result my_result) {
+    result1 = my_result.result1;
+    result2 = my_result.result2;
+  }
+};
+
+double_result find_diviation (axis x, axis y){
+   double_result sumup;
+   sumup.result1 = x.check_axis_orientary();
+   sumup.result2 = y.check_axis_orientary();
+    return sumup;
+}
+
+const float Baseline = 100;                 // distance between the transducers (aka c - sight) (cm)
 
 class resisto_coil {
   
@@ -57,6 +121,13 @@ public:
   inc = my_inc;
   cs = my_cs;
  }
+
+  	void boot () {
+ 		pinMode(inc, OUTPUT);
+		pinMode(ud, OUTPUT);
+		pinMode(cs, OUTPUT);
+		digitalWrite(cs, HIGH);
+ 	}
 
  void boost_coil() {                   // 1step resistor boost
   
@@ -79,8 +150,8 @@ public:
 
  void buck_coil() {                         // 1step buck resistor
  
-  digitalWrite(ud, LOW);        //  U/D  goes up 1
-  digitalWrite(inc, HIGH);        //  INC too
+  digitalWrite(ud, LOW);        //  U/D  goes down 1
+  digitalWrite(inc, HIGH);      //  INC too
   digitalWrite(cs, LOW);        // turn on
  
   delayMicroseconds(1);
@@ -104,70 +175,36 @@ public:
               magnet_object2 (Up2, Inc2, Cs2), // for X axis
               magnet_object3 (Up3, Inc3, Cs3),
               magnet_object4 (Up4, Inc4, Cs4); // for Y axis
-
- class axis { // triangle sights a & b ; c - const
-
-  int Adist;
-  int Bdist;
-  Ultrasonic *ultra_object1;
-  Ultrasonic *ultra_object2;
-
-public:
-
-  void read_A (){
-    Adist = ultra_object1->distanceRead();
-  }
-
-  void read_B (){
-    Bdist = ultra_object2->distanceRead();
-  }
-
-  
-  axis(Ultrasonic &ultra_object1, Ultrasonic &ultra_object2) {
-    read_A();
-    read_B();
-
-    } // here is some kinda of initiative
-
-  int check_axis_orientary () { // returns the axis inclination
-    return (Adist - Bdist);
-  }
-
- };
-
-struct double_result {
-  int result1;
-  int result2;
-
-  const double_result operator = (double_result my_result) {
-    result1 = my_result.result1;
-    result2 = my_result.result2;
-  }
-};
-
-const float Baseline = 100;                 // distance between the transducers (aka c - sight) (cm)
  
  // globals ***************************************
 
+void correction (axis &x, axis &y) {
+ 	int Xdiviation = x.check_axis_orientary();
+ 	int Ydiviation = y.check_axis_orientary();
+    
+    while (true){
+    	//if (x > 0) magnet_object1.b
+
+    }
+
+ }
+
+
+
+
 void setup () {
-  Serial.begin(9600);  
-  
+	Serial.begin(9600);  
+	
+	// now we need to boot up the magnet objects:
+
+	magnet_object1.boot();
+	magnet_object2.boot();
+	magnet_object3.boot();
+	magnet_object4.boot();
+
 }
 
 void loop () {
 
-  axis x (ultrasonic1, ultrasonic2);
-  axis y (ultrasonic3, ultrasonic4);
-  
-  
-  double_result bias;
-  bias = find_diviation (x, y);
-}
-
-double_result find_diviation (axis x, axis y){
-   double_result sumup;
-   sumup.result1 = x.check_axis_orientary();
-   sumup.result2 = y.check_axis_orientary();
-    return sumup;
 }
 
