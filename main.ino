991da89d740b8b0756ce9a29e75ce2ec7cbaed94
kerusey <38439184+kerusey@users.noreply.github.com>
2019-06-29 08:12:04 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,4 +1,4 @@
-/*  !!! important stuff:
+/*	!!! important stuff:
 
 magnet_objects 1 and 2 used for X axis (right) (left)
 magnet_objects 3 and 4 used for Y axis (right) (left)
@@ -47,10 +47,11 @@ ultrasonic objects 3 and 4 used for Y axis (right) (left)
 #define HallS1 21
 #define HallS2 22
 #define HallS3 23
-#define HallS4 24         // 4 hall sensor's ports
+#define HallS4 24					// 4 hall sensor's ports
 
 
 #define const_resistance 3000
+#define const_alpha 30
 
  // 4 ultrasonic objects
  
@@ -88,12 +89,12 @@ public:
     } // constructor
 
   int check_axis_orientary () { // returns the axis inclination (from the right sight)
-    return (Adist - Bdist); 
+    return (Adist*cos(const_alpha) - Bdist*cos(const_alpha)); 
   }
 
  };
 
-/* struct double_result {
+struct double_result {
   int result1;
   int result2;
 
@@ -101,14 +102,14 @@ public:
     result1 = my_result.result1;
     result2 = my_result.result2;
   }
-}; */
+};
 
-/* double_result find_diviation (axis x, axis y){
+double_result find_diviation (axis x, axis y){
    double_result sumup;
    sumup.result1 = x.check_axis_orientary();
    sumup.result2 = y.check_axis_orientary();
     return sumup;
-} */
+}
 
 const float Baseline = 100;                 // distance between the transducers (aka c - sight) (cm)
 
@@ -128,12 +129,12 @@ public:
   cs = my_cs;
  }
 
-    void boot () {
-    pinMode(inc, OUTPUT);
-    pinMode(ud, OUTPUT);
-    pinMode(cs, OUTPUT);
-    digitalWrite(cs, HIGH);
-  }
+  	void boot () {
+ 		pinMode(inc, OUTPUT);
+		pinMode(ud, OUTPUT);
+		pinMode(cs, OUTPUT);
+		digitalWrite(cs, HIGH);
+ 	}
 
  void boost_coil() {                   // 1step resistor boost
   
@@ -185,80 +186,80 @@ public:
  // globals ***************************************
 
 bool hall_check (){  
-  if (digitalRead(HallS1) == LOW || digitalRead(HallS2) == LOW || digitalRead(HallS3) == LOW || digitalRead(HallS4) == LOW) 
-    return true;
-  else return false;
+	if (digitalRead(HallS1) == LOW || digitalRead(HallS2) == LOW || digitalRead(HallS3) == LOW || digitalRead(HallS4) == LOW) 
+		return true;
+	else return false;
 } 
 
 
 void correction (axis &x, axis &y) { // this is the main function of control coils
-  
-  int Xdiviation = x.check_axis_orientary();
-  int Ydiviation = y.check_axis_orientary();
+ 	
+ 	int Xdiviation = x.check_axis_orientary();
+ 	int Ydiviation = y.check_axis_orientary();
     
     while (true) {
-      
-      // check X axis deviation
+    	
+    	// check X axis deviation
 
-        while (Xdiviation > 0) {  // check deviation of right sight
-          magnet_object1.buck_coil();
-          magnet_object2.boost_coil();
-        }
+    		while (Xdiviation > 0) {  // check deviation of right sight
+    			magnet_object1.buck_coil();
+    			magnet_object2.boost_coil();
+    		}
 
-        while (Xdiviation < 0) {  // check deviation of left sight
-          magnet_object1.boost_coil();
-          magnet_object2.buck_coil();
-          }
+    		while (Xdiviation < 0) {  // check deviation of left sight
+    			magnet_object1.boost_coil();
+    			magnet_object2.buck_coil();
+    			}
 
-        while (Xdiviation == 0) {
-          Xdiviation = x.check_axis_orientary();
-          delay(100); // variable
-        }
+    		while (Xdiviation == 0) {
+    			Xdiviation = x.check_axis_orientary();
+    			delay(100); // variable
+    		}
 
-          // end of checking X axis
-    
-    // check Y axis deviation
-      
-        while (Ydiviation > 0) {  // check deviation of right sight
-          magnet_object3.buck_coil();
-          magnet_object4.boost_coil();
-        }
+    			// end of checking X axis
+ 		
+ 		// check Y axis deviation
+    	
+    		while (Ydiviation > 0) {  // check deviation of right sight
+    			magnet_object3.buck_coil();
+    			magnet_object4.boost_coil();
+    		}
 
-        while (Ydiviation < 0) {  // check deviation of left sight
-          magnet_object3.boost_coil();
-          magnet_object4.buck_coil(); 
-          }
+    		while (Ydiviation < 0) {  // check deviation of left sight
+    			magnet_object3.boost_coil();
+    			magnet_object4.buck_coil(); 
+    			}
 
-        while (Ydiviation == 0) { 
-          Ydiviation = y.check_axis_orientary();
-          delay(100); // variable
-        }
+    		while (Ydiviation == 0) { 
+    			Ydiviation = y.check_axis_orientary();
+    			delay(100); // variable
+    		}
 
-          // end of checking Y axis 
+    			// end of checking Y axis 
 
-        Xdiviation = x.check_axis_orientary();
-        Ydiviation = y.check_axis_orientary();
+    		Xdiviation = x.check_axis_orientary();
+    		Ydiviation = y.check_axis_orientary();
 
-        delay(100); // variable
+    		delay(100); // variable
 
-          while (hall_check) delay(3000); // checks if construction fall
-      }
+    			while (hall_check) delay(3000); // checks if construction fall
+    	}
 
  }
 
 
+
+
 void setup () {
-  Serial.begin(9600);  
-  
-  // now we need to boot up the magnet objects:
+	Serial.begin(9600);  
+	
+	// now we need to boot up the magnet objects:
 
-  magnet_object1.boot();
-  magnet_object2.boot();
-  magnet_object3.boot();
-  magnet_object4.boot();
+	magnet_object1.boot();
+	magnet_object2.boot();
+	magnet_object3.boot();
+	magnet_object4.boot();
 
-  // and change hall sensor's pin mode to input
-  
     pinMode(HallS1, INPUT);
     pinMode(HallS2, INPUT);
     pinMode(HallS3, INPUT);
@@ -267,10 +268,9 @@ void setup () {
 }
 
 void loop () {
-  axis x (ultrasonic_object1, ultrasonic_object2),
-     y (ultrasonic_object3, ultrasonic_object4);
+	axis x (ultrasonic_object1, ultrasonic_object2),
+		 y (ultrasonic_object3, ultrasonic_object4);
 
-     correction (x, y);
-
+		 correction (x, y);
 }
 
