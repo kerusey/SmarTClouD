99317dd1c3f122f603b811e58be8de9482dc5efa
kerusey <38439184+kerusey@users.noreply.github.com>
2019-07-07 10:24:33 -0400

    Update Levitron.ino

--- Levitron.ino
+++ Levitron.ino
@@ -8,36 +8,36 @@ ultrasonic objects 3 and 4 used for Y axis (right) (left)
  // you may also need a high variable "h"
 
 #include "Ultrasonic.h"
-
+#include <SPI.h> // i dont know shall we use it or not (is there any protocols (not funcs and methods!), that this lib supports)
  // globals ***************************************
+// ports ***********************************************
+#define Trig1 50                       // sensor A "Trig" pin
+#define Echo1 51                       // sensor A "Echo" pin
 
-#define Trig1 1                       // sensor A "Trig" pin
-#define Echo1 2                       // sensor A "Echo" pin
+#define Trig2 52                       // sensor B "Trig" pin
+#define Echo2 53                       // sensor B "Echo" pin
 
-#define Trig2 3                       // sensor B "Trig" pin
-#define Echo2 4                       // sensor B "Echo" pin
+#define Trig3 54                       // sensor C "Trig" pin
+#define Echo3 55                       // sensor C "Echo" pin
 
-#define Trig3 5                       // sensor C "Trig" pin
-#define Echo3 6                       // sensor C "Echo" pin
+#define Trig4 56                      // sensor D "Trig" pin
+#define Echo4 57                      // sensor D "Echo" pin
 
-#define Trig4 7                      // sensor D "Trig" pin
-#define Echo4 8                      // sensor D "Echo" pin
+#define SDI_1 7
+#define CLK_1 8
 
-#define Up1 9
-#define Inc1 10
-#define Cs1 11
+#define SDI_2 35
+#define CLK_2 36
 
-#define Up2 12
-#define Inc2 13
-#define Cs2 14
+#define SDI_3 58
+#define CLK_3 59
 
-#define Up3 15
-#define Inc3 16
-#define Cs3 17
+#define SDI_4 70
+#define CLK_4 71
 
-#define Up4 18
-#define Inc4 19
-#define Cs4 20                      // inititializes 4 resisto_coil objects
+#define CS 9                      // const ports for 4 resisto_coil objects
+
+// ports **********************************************
 
 #define HallS1 21
 #define HallS2 22
@@ -45,10 +45,10 @@ ultrasonic objects 3 and 4 used for Y axis (right) (left)
 #define HallS4 24         // 4 hall sensor's ports
 
 
-#define const_resistance 10.0           // variable default resistance of coils (better be over 10; best > const_delta_resistance * 10)
-#define const_alpha 30                  // variable (ultrasonic object's angle)
+#define const_resistance 10.0         // variable default resistance of coils (better be over 10; best > const_delta_resistance * 10)
+#define const_alpha 30                // variable (ultrasonic object's angle)
 #define const_delta_resistance 3.906  // variable, equals step of resistor (in example used AD8400, with a step 1000/256 Ohm/step) 
-
+#define max_coil_resistance 20        // could be variable, depending on your coil resistance (const max_coil_resistance = 2*coil_resistance), shows maximal approved resistance for resistor (paralleled coil) 
 
  // 4 ultrasonic objects
  
@@ -77,7 +77,6 @@ public:
   void read_B (){
     Bdist = ultra_object2->distanceRead();
   }
-
   
   axis(Ultrasonic &ultra_object1, Ultrasonic &ultra_object2) {
     read_A();
@@ -91,7 +90,8 @@ public:
 
  };
 
-class squad_container {  // used here to contain resistance before hall_check func change it
+/*
+class squad_container {  // used here to contain resistance before hall_check func change it (uleless in futuere)
   float squad_member1;
   float squad_member2;
   float squad_member3;
@@ -113,85 +113,77 @@ public:
       *sm4 = squad_member4;
     }
 };
-
+*/
 
 const float Baseline = 100;                 // distance between the transducers (aka c - sight) (cm)
 
 class resisto_coil {
   
-  int ud;
-  int inc;
-  int cs;
+  int sdi;
+  int clk;
+  int cs;  // ports for resistor pci
   float resistance;
 
 public:
 
- resisto_coil (int my_ud, int my_inc, int my_cs) { // my_ input must be constant !!!
+ resisto_coil (int my_sdi, int my_clk) { // my_ input must be constant !!!
   resistance = const_resistance;
-  ud = my_ud;
-  inc = my_inc;
-  cs = my_cs;
+  sdi = my_sdi;
+  clk = my_clk;
+  cs = CS;
  }
 
   void boot () {
-    pinMode(inc, OUTPUT);
-    pinMode(ud, OUTPUT);
+    pinMode(sdi, OUTPUT);
+    pinMode(clk, OUTPUT);
     pinMode(cs, OUTPUT);
     digitalWrite(cs, HIGH);
   }
 
   int coil_resistance () {return resistance;}
 
-  void boost_coil (size_t delta = 1) {                   // 1step resistor boost
-    for (size_t i = 0 ; i < delta ; ++i) {
-      digitalWrite(ud, HIGH);             //  U/D  goes up 1
-      digitalWrite(inc, HIGH);            //  INC too
-      digitalWrite(cs, LOW);              // turn on
+  void change_coil_resistance (int res) {
+
+      digitalWrite(cs, HIGH);             
+      digitalWrite(sdi, HIGH);            
+      digitalWrite(clk, LOW);             
       
       delayMicroseconds(1);               
       
-      digitalWrite(inc, LOW);             
+      digitalWrite(cs, LOW);             
       
       delayMicroseconds(1);
       
-      digitalWrite(inc, HIGH);
+      digitalWrite(sdi, LOW);
       
       delayMicroseconds(1);
 
-      digitalWrite(cs, HIGH);             // turn off and runs EEPROM
-      resistance += const_delta_resistance;                   
+      for (size_t i = 0 ; i < 2 ; ++i) {
+        digitalWrite (clk, LOW);
+        digitalWrite (clk, HIGH);
+      }
+
+      for (size_t i = 7 ; i >= 0 ; --i){
+        digitalWrite (clk, LOW);
+        
+        int bt = bitRead(res, i);
+        
+        if (bt == 1) digitalWrite (sdi, HIGH);
+        else digitalWrite (sdi, LOW);
+
+        digitalWrite (clk, HIGH);
+      }
+      digitalWrite (cs, HIGH);
     }
- }
-
-  void buck_coil (size_t delta = 1) {                         // 1step buck resistor
-    for (size_t i = 0 ; i < delta ; ++i){
-      digitalWrite(ud, LOW);        //  U/D  goes down 1
-      digitalWrite(inc, HIGH);      //  INC too
-      digitalWrite(cs, LOW);        // turn on
-     
-      delayMicroseconds(1);
-     
-      digitalWrite(inc, LOW);
-     
-      delayMicroseconds(1);
-     
-      digitalWrite(inc, HIGH);
-     
-      delayMicroseconds(1);
-
-      digitalWrite(cs, HIGH);
-      resistance -= const_delta_resistance;
-    }
-  }
 
 };
 
 // now creating a 4 megnetic coil objects, been controlled by variable resistor;
  
- resisto_coil magnet_object1 (Up1, Inc1, Cs1),
-              magnet_object2 (Up2, Inc2, Cs2), // for X axis
-              magnet_object3 (Up3, Inc3, Cs3),
-              magnet_object4 (Up4, Inc4, Cs4); // for Y axis
+ resisto_coil magnet_object1 (SDI_1, CLK_1),
+              magnet_object2 (SDI_2, CLK_2), // for X axis
+              magnet_object3 (SDI_3, CLK_3),
+              magnet_object4 (SDI_4, CLK_4); // for Y axis
  
  // globals ***************************************
 
@@ -212,14 +204,14 @@ void correction (axis &x, axis &y) { // this is the main function of control coi
       // check X axis deviation
         if (!hall_check) {
           while (Xdiviation > 0) {  // check deviation of right sight
-            magnet_object1.buck_coil();
-            magnet_object2.boost_coil();
+            if (magnet_object1.coil_resistance() >= const_delta_resistance) magnet_object1.change_coil_resistance(-1); // buck
+            if (magnet_object2.coil_resistance() < max_coil_resistance) magnet_object2.change_coil_resistance(1); // boost
           }
 
           while (Xdiviation < 0) {  // check deviation of left sight
-            magnet_object1.boost_coil();
-            magnet_object2.buck_coil();
-            }
+            if (magnet_object1.coil_resistance() >= const_delta_resistance) magnet_object1.change_coil_resistance(1); // boost 
+            if (magnet_object2.coil_resistance() < max_coil_resistance) magnet_object2.change_coil_resistance(-1); // buck
+          }
 
           while (Xdiviation == 0) {
             Xdiviation = x.check_axis_orientary();
@@ -231,13 +223,13 @@ void correction (axis &x, axis &y) { // this is the main function of control coi
       // check Y axis deviation
         
           while (Ydiviation > 0) {  // check deviation of right sight
-            magnet_object3.buck_coil();
-            magnet_object4.boost_coil();
+            if (magnet_object3.coil_resistance() >= const_delta_resistance) magnet_object3.change_coil_resistance(-1); // buck
+            if (magnet_object4.coil_resistance() < max_coil_resistance) magnet_object4.change_coil_resistance(1); // boost
           }
 
           while (Ydiviation < 0) {  // check deviation of left sight
-            magnet_object3.boost_coil();
-            magnet_object4.buck_coil(); 
+            if (magnet_object3.coil_resistance() >= const_delta_resistance) magnet_object3.change_coil_resistance(1); // boost
+            if (magnet_object4.coil_resistance() < max_coil_resistance) magnet_object4.change_coil_resistance(-1); // buck
             }
 
           while (Ydiviation == 0) { 
@@ -254,13 +246,13 @@ void correction (axis &x, axis &y) { // this is the main function of control coi
         }
 /* now we need to read hall sensor's input */
       else { // if hall_check == true 
-          squad_container my_sq_container;
-          my_sq_container.import_sq (magnet_object1.coil_resistance(), magnet_object2.coil_resistance(), magnet_object3.coil_resistance(), magnet_object4.coil_resistance()); // import all resistance of each coil before they'll be changed
+    //      squad_container my_sq_container;
+  //        my_sq_container.import_sq (magnet_object1.coil_resistance(), magnet_object2.coil_resistance(), magnet_object3.coil_resistance(), magnet_object4.coil_resistance()); // import all resistance of each coil before they'll be changed
  
-          magnet_object1.buck_coil(int(magnet_object1.coil_resistance()));
-          magnet_object2.buck_coil(int(magnet_object2.coil_resistance()));
-          magnet_object3.buck_coil(int(magnet_object3.coil_resistance()));
-          magnet_object4.buck_coil(int(magnet_object4.coil_resistance()));
+          magnet_object1.change_coil_resistance (-int(magnet_object1.coil_resistance()));
+          magnet_object2.change_coil_resistance (-int(magnet_object2.coil_resistance()));
+          magnet_object3.change_coil_resistance (-int(magnet_object3.coil_resistance()));
+          magnet_object4.change_coil_resistance (-int(magnet_object4.coil_resistance()));
 
         /** little summary. We have imported resistances to squad container*/
           do
@@ -268,12 +260,12 @@ void correction (axis &x, axis &y) { // this is the main function of control coi
           while (hall_check); // now we've waited for as much time as user needs to take levitating object away from coils
           // and we need to boot'em up again
 
-          my_sq_container.export_sq (magnet_object1.coil_resistance(), magnet_object2.coil_resistance(), magnet_object3.coil_resistance(), magnet_object4.coil_resistance());  // returns values of resistance of each coil back
+//          my_sq_container.export_sq (magnet_object1.coil_resistance(), magnet_object2.coil_resistance(), magnet_object3.coil_resistance(), magnet_object4.coil_resistance());  // returns values of resistance of each coil back
           
-          magnet_object1.boost_coil (int(magnet_object1.coil_resistance()));
-          magnet_object2.boost_coil (int(magnet_object2.coil_resistance()));
-          magnet_object3.boost_coil (int(magnet_object3.coil_resistance()));
-          magnet_object4.boost_coil (int(magnet_object4.coil_resistance()));
+          magnet_object1.change_coil_resistance (const_resistance);
+          magnet_object2.change_coil_resistance (const_resistance);
+          magnet_object3.change_coil_resistance (const_resistance);
+          magnet_object4.change_coil_resistance (const_resistance);  //! ALL THOSE COMMENTS ARE BECAUSE I WANTED TO CONTAIN VALUES OF EACH COIL RESISTANCE
           
           // here we go back
 
