83f28662d4ac654a7543ebd419b95d6c52400871
kerusey <38439184+kerusey@users.noreply.github.com>
2019-09-06 14:56:54 +0300

    Update PWM_controller.cpp

--- Levitron/PWM_controller.cpp
+++ Levitron/PWM_controller.cpp
@@ -8,9 +8,20 @@ int convert_percent_to_byte () {
   return int(percent*100/255);
 }
 
-void set_voltage (double _current_voltage = default_voltage, unsigned int _current_percent = NULL, int _current_byte = NULL);
-void buck (double _const_delta_voltage = const_delta_voltage);
-void boost (double _const_delta_voltage = const_delta_voltage);
-pwm_coil ();
+void set_voltage (double _current_voltage = default_voltage, unsigned int _current_percent = NULL, int _current_byte = NULL) {
 
-~pwm_coil ();
+}
+
+void buck (double _const_delta_voltage = const_delta_voltage) {
+
+}
+
+void boost (double _const_delta_voltage = const_delta_voltage) {
+
+}
+
+pwm_coil (unsigned int _set_pin) {
+  analogWrite(_set_pin, );
+}
+
+~pwm_coil () {}
