3090f2d81040835ce4f7d550940d187417592b76
kerusey <38439184+kerusey@users.noreply.github.com>
2019-10-03 14:55:10 +0300

    Update axis.cpp

--- Levitron/axis.cpp
+++ Levitron/axis.cpp
@@ -16,6 +16,10 @@ void axis::read_B () {
   Bdist = ultra_object2->read();
 }
 
+void axis::read_height () {
+  height = ultra_height->read();
+}  // height is unique (not common for each class)
+
 axis::axis(Ultrasonic &ultra_object1, Ultrasonic &ultra_object2) {
   read_A();
   read_B();
@@ -23,7 +27,7 @@ axis::axis(Ultrasonic &ultra_object1, Ultrasonic &ultra_object2) {
 } // constructor
 
 double axis::check_axis_orientary () { // returns the axis inclination (from the right sight)
-  return (Adist * cos(const_alpha) - Bdist * cos(const_alpha));
+  return 
 }
 
 void axis::get_status() {
