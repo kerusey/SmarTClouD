d4d40dfc607cd3a7af96bf7fedaef743f2f8f0cb
kerusey <38439184+kerusey@users.noreply.github.com>
2019-10-08 11:42:56 +0300

    Update axis.cpp

--- Levitron/axis.cpp
+++ Levitron/axis.cpp
@@ -16,12 +16,12 @@ Axis::axis(Ultrasonic &ultra_object1, Ultrasonic &ultra_object2) {
 
 bool Axis::check_right_half () { // returns true if not inclined to right
   if (!global_protect() || !st::get_status())
-    return L1_dist <= sqrt ( height * height + sqr ((baseline - magnet_body_lenght) / 2;
+    return L1_dist <= sqrt ( height * height + sqr ((baseline - magnet_body_lenght) / 2));
 }
 
 bool Axis::check_left_half () { // returns true if not inclined to left
   if (!global_protect() || !st::get_status())
-    return L2_dist <= sqrt ( height * height + sqr ((baseline - magnet_body_lenght) / 2;
+    return L2_dist <= sqrt ( height * height + sqr ((baseline - magnet_body_lenght) / 2));
 }
 
 int Axis::check_axis_orientary () { // returns  abstract points of incline (positive - to right ; negative - to left)
